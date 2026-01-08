b7423d643dcdf6ca1fdf3ec8a80b7d6e09602889
Tony Salvador <tonilsonsalvador026@gmail.com>
2026-01-08 16:29:31 +0100

    fix: remove duplicated backend and keep mysql prisma

--- backend/prisma/migrations/20250919124631_add_nif_to_proprietario/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."Inquilino" ADD COLUMN     "nif" TEXT;
-
--- AlterTable
-ALTER TABLE "public"."Proprietario" ADD COLUMN     "nif" TEXT;
--- prisma/migrations/20250925122244_add_ativo_to_pagamento/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."Pagamento" ADD COLUMN     "ativo" BOOLEAN NOT NULL DEFAULT true;
